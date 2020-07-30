96d58db42868916d4db62560ab2a75ba385305bc
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-07-30 17:47:11 -0500

    Implemented seller  submit button

---
+++ public/js/seller.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+  $("#sellerSubBtn").click(function () {
+    const name = $("#name").val()? $("#name").val().trim(): "";
+    const image = $("#image").val()? $("#image").val().trim(): "";
+    const desc = $("#desc").val()? $("#desc").val().trim(): "";
+
+    // if (!name || !image) {
+    //  return;
+    //} 
+
+    const data = {
+      name,
+      image,
+      desc
+    };
+
+    console.log("seller data", data);
+
+    $.post(
+      "/api/rock/sellerData",
+      data
+    )
+      .then(function () {
+        window.location.href = "/members.html";
+      }).catch(sellerdataErr => {
+        console.log("Error submitting form", sellerdataErr);
+      });
+  });
+
+});
