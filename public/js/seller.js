67faeed894e3184df49f6b8ad97ecdf662eb5be0
Christian Miller <cnmiller127@gmail.com>
2020-07-30 12:03:57 -0500

    routing

--- public/js/seller.js
+++ public/js/seller.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   //Getting references from seller.html page
   var sellerForm = $("form.sellerForm");
-  //var rockName = $("input#Rock_name");
-  //var rockImage = $("input#url");
-  //var Desc = $("textarea.materialize-textarea");
+  var rockName = $("input#Rock_name");
+  var rockImage = $("input#url");
+  var Desc = $("textarea.materialize-textarea");
   // When the signup button is clicked, we validate the email and password are not blank
   sellerForm.on("submit", function(event) {
     event.preventDefault();
-    //var sellerData = {
-     // rockName: rockName.val().trim(),
-     // rockImage: rockImage.val().trim(),
-     // Desc: Desc.val().trim()
-    //};
-
+    var sellerData = {
+      rockName: rockName.val().trim(),
+      rockImage: rockImage.val().trim(),
+      Desc: Desc.val().trim()
+    };
+    
     // Does a post to the sellerdata route. If successful, we are redirected to the members page
     // Otherwise we log any errors
     function sellerData(rockName, rockImage, Desc) {
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -49,13 +49,13 @@ module.exports = function(app) {
   // NEW POSTS
   // get all posted rocks. We want user to be able to see ALL posts( their own and others)
   app.get("/api/rock/sellerDataAll", function(req, res) {
-    db.TradeReq.findAll({
+    db.Rock.findAll({
       where: {
         posted: true
       }
     })
-      .then(function(dbTradeReq) {
-        res.json(dbTradeReq);
+      .then(function(dbRock) {
+        res.json(dbRock);
       })
       .catch(function() {
         res.status(500).end();
@@ -89,14 +89,14 @@ module.exports = function(app) {
   });
   // allow a logged in user to post new rock
   app.post("/api/rock/sellerData", function(req, res) {
-    db.TradeReq.create(req.body, { include: User })
+    db.Rock.create(req.body, { include: User })
       .then(function(dbPost) {
         res.json(dbPost);
       })
       .catch(err => console.log(err));
   });
   // allow user to update their post
-  // app.put("/api/")
+  // app.put("/api/user/sellerData")
   //allow user to delete themselves
   app.delete("/api/users/:id", function(req, res) {
     db.User.destroy({
