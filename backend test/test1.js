da2d91041f6359ad56ce2ae291a76d76e9d2b97a
Christian Miller <cnmiller127@gmail.com>
2020-07-31 10:56:52 -0500

    description added

--- backend test/test1.js
+++
@@ -1,5 +0,0 @@
-var db = require("../models");
-console.log("fuck");
-// var rock = {name: "geodude", image: "img.jpg", posted: true};
-// db.Rock.create(rock).catch(err => console.log(err));
-// console.log("FUCK");
